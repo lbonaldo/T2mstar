c14c80d511d953397f3805b05e2f98eb4a5e2f76
lbonaldo <bonaldo.luca12@gmail.com>
2021-11-21 20:47:39 -0500

    testing datasets

--- exploration.jl
+++ exploration.jl
@@ -1,30 +1,22 @@
-# (inverse) inverse> $env:JULIA_NUM_THREADS=4
-# (inverse) inverse> julia --project=. .\julia_to_py_dataset.jl
-
-using Random
-using PyCall
+using Statistics
 using JLD2: @load
-np = pyimport("numpy")
-
 train_file = "data/train_data.out"
 test_file = "data/test_data.out"
 @load train_file traindata
 @load test_file testdata
-
-xmin = 0.05
-dx = 0.01
-xmax = 5.0
-beta = collect(xmin:dx:xmax)
 
 frac = 0.1
 num_rows = Int64(round(length(traindata)*frac))
 num_column = length(traindata[1][1])
 num_params = length(traindata[1][2])+1
 
-println(num_rows)
-
 x_train = Array{Float64,2}(undef, num_rows*num_column, num_params)
 y_train = Array{Float64,1}(undef, num_rows*num_column)
+
+xmin = 0.05
+dx = 0.01
+xmax = 5.0
+beta = collect(xmin:dx:xmax)
 
 Threads.@threads for i in 1:num_rows
     for j in 1:num_column
@@ -33,24 +25,18 @@ Threads.@threads for i in 1:num_rows
     end
 end
 
-rng = MersenneTwister(1234);
-x_train = x_train[shuffle(rng, 1:end), :]
-shuffle!(rng, y_train)
-
-data = np.asarray(x_train)
-np.save("data/coeff_train.npy",data)
-data = np.asarray(y_train)
-np.save("data/I_train.npy",data)
-
-num_rows = Int64(round(length(testdata)))-1
+frac = 0.1
+num_rows = Int64(round(length(testdata)*frac))
 num_column = length(testdata[1][1])
 num_params = length(testdata[1][2])+1
-
-println(num_rows)
 
 x_test = Array{Float64,2}(undef, num_rows*num_column, num_params)
 y_test = Array{Float64,1}(undef, num_rows*num_column)
 
+xmin = 0.05
+dx = 0.01
+xmax = 5.0
+beta = collect(xmin:dx:xmax)
 
 Threads.@threads for i in 1:num_rows
     for j in 1:num_column
@@ -59,7 +45,13 @@ Threads.@threads for i in 1:num_rows
     end
 end
 
-data = np.asarray(x_test)
-np.save("data/coeff_test.npy",data)
-data = np.asarray(y_test)
-np.save("data/I_test.npy",data)
+println(mean!([1. 1. 1. 1. 1. 1.], x_train))
+println(mean!([1. 1. 1. 1. 1. 1.], x_test))
+println(std(x_train[:,1]), "\t", std(x_test[:,1]))
+println(std(x_train[:,2]), "\t", std(x_test[:,2]))
+println(std(x_train[:,3]), "\t", std(x_test[:,3]))
+println(std(x_train[:,4]), "\t", std(x_test[:,4]))
+println(std(x_train[:,5]), "\t", std(x_test[:,5]))
+println(std(x_train[:,6]), "\t", std(x_test[:,6]))
+println(mean(y_train), "\t", mean(y_test))
+println(std(y_train), "\t", std(y_test))
